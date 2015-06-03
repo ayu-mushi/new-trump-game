8c773350fd536991f11182ebd73c70e1bb04c927
ayu-mushi <ayu.mushi2015@gmail.com>
2015-06-03 09:04:05 +0900

    s/elm/e/g

--- JQuery.hs
+++ JQuery.hs
@@ -16,18 +16,18 @@ ffi' = ffi . toJSString
 
 childrenEl :: JQuery -> IO JQuery
 childrenEl domEl = do 
-  elem <- ffi' "(function(domEl){ return domEl.children() })" $ fromJQuery domEl
-  return $ JQuery elem
+  e <- ffi' "(function(domEl){ return domEl.children() })" $ fromJQuery domEl
+  return $ JQuery e
 
 nextEl :: JQuery -> IO JQuery
 nextEl domEl = do 
-  elem <- ffi' "(function(domEl){ return domEl.next() })" $ fromJQuery domEl
-  return $ JQuery elem
+  e <- ffi' "(function(domEl){ return domEl.next() })" $ fromJQuery domEl
+  return $ JQuery e
 
 prevEl :: JQuery -> IO JQuery
 prevEl domEl = do 
-  elem <- ffi' "(function(domEl){ return domEl.prev() })" $ fromJQuery domEl
-  return $ JQuery elem
+  e <- ffi' "(function(domEl){ return domEl.prev() })" $ fromJQuery domEl
+  return $ JQuery e
 
 whenClick :: JQuery -> IO () -> IO ()
 whenClick = ffi' "(function(domEl, f){ domEl.click(f) })" . fromJQuery
