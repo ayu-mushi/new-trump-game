6f05cff0a145954fca3274a7d70572c41a5d9fff
ayu-mushi <ayu.mushi2015@gmail.com>
2015-06-05 02:58:59 +0900

    add isExistEl

--- JQuery.hs
+++ JQuery.hs
@@ -14,20 +14,27 @@ newtype JQuery = JQuery { fromJQuery :: Elem }
 ffi' :: FFI a => String -> a
 ffi' = ffi . toJSString
 
-childrenEl :: JQuery -> IO JQuery
+-- "jQueryによる要素の存在チェックまとめ: 小粋空間" http://www.koikikukan.com/archives/2012/04/18-022222.php
+isExistEl :: Elem -> IO Bool
+isExistEl = ffi' "(function(domEl){return domEl[0]?true:false})"
+
+childrenEl :: JQuery -> IO (Maybe JQuery)
 childrenEl domEl = do 
   e <- ffi' "(function(domEl){ return domEl.children() })" $ fromJQuery domEl
-  return $ JQuery e
+  p <- isExistEl e
+  return $ if p then Just $ JQuery e else Nothing
 
-nextEl :: JQuery -> IO JQuery
+nextEl :: JQuery -> IO (Maybe JQuery)
 nextEl domEl = do 
   e <- ffi' "(function(domEl){ return domEl.next() })" $ fromJQuery domEl
-  return $ JQuery e
+  p <- isExistEl e
+  return $ if p then Just $ JQuery e else Nothing
 
-prevEl :: JQuery -> IO JQuery
+prevEl :: JQuery -> IO (Maybe JQuery)
 prevEl domEl = do 
   e <- ffi' "(function(domEl){ return domEl.prev() })" $ fromJQuery domEl
-  return $ JQuery e
+  p <- isExistEl e
+  return $ if p then Just $ JQuery e else Nothing
 
 whenClick :: JQuery -> IO () -> IO ()
 whenClick = ffi' "(function(domEl, f){ domEl.click(f) })" . fromJQuery
