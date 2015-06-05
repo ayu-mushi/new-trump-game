316505c91cd2fd1f4058c152237d0d2611424fa6
ayu-mushi <ayu.mushi2015@gmail.com>
2015-06-06 07:55:05 +0900

    add a function 'appendEl'

--- JQuery.hs
+++ JQuery.hs
@@ -43,6 +43,9 @@ prevEl domEl = fmap ToJQuery $
 whenClick :: JQuery -> (DOMObj -> IO ()) -> IO ()
 whenClick domEl method = ffi' "(function(domEl, f){ domEl.click(function(){ f(this) }) })" (fromJQuery domEl) $ method . ToDOMObj
 
+appendEl :: String -> JQuery -> IO ()
+appendEl str = ffi' "(function(str, domEl){ domEl.append(str) })" str . fromJQuery
+
 indexEl :: JQuery -> DOMObj -> IO (Maybe Int)
 indexEl domEl subject = fmap (\n -> if n == -1 then Nothing else Just n)
   $ ffi' "(function(domEl, subj){ return domEl.index(subj) })" (fromJQuery domEl) $ fromDOMObj subject
