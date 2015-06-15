812e6f14b12e604eb79d91fd96a53228f014e566
ayu-mushi <ayu.mushi2015@gmail.com>
2015-06-16 01:43:01 +0900

    add a Perch function 'prevNode'

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 import Data.Array (Array(Array))
 import System.Random.Shuffle (shuffleM)
-import Haste (mkCallback, alert, JSFun(JSFun))
+import Haste (mkCallback, alert, JSFun(JSFun), Elem, toJSString)
+import Haste.Foreign (ffi)
 import qualified Haste.Perch as P
 
 import Cards
@@ -19,6 +20,12 @@ data Game = Game {
 
 turnPlayer :: (Bool, Player, Player) -> Player
 turnPlayer (p, a, b) = if p then a else b
+
+prevNode :: P.Perch
+prevNode = P.Perch $ ffi $ toJSString "function(node){ return node.previousSibling }"
+
+indexEl :: P.Perch -> P.Perch -> IO (Maybe Int)
+indexEl = undefined
 
 foreign import ccall "setInterval" timeout :: JSFun (IO ()) -> Int -> IO ()
 
