ed2e3873ace9f1e805f783456364ca88edcd7872
ayu-mushi <ayu.mushi2015@gmail.com>
2015-06-18 07:58:33 +0900

    add some functions

--- Data/List/Zipper.hs
+++ Data/List/Zipper.hs
@@ -1,4 +1,18 @@
 module Data.List.Zipper
   (Zipper) where
 
+import Control.Lens
+
 data Zipper a = Zip [a] [a]
+
+empty :: Zipper a
+empty = Zip [] []
+
+fromList :: [a] -> Zipper a
+fromList as = Zip [] as
+
+toList :: Zipper a -> [a]
+toList (Zip ls rs) = reverse ls ++ rs
+
+cursor :: Lens' (Zipper a) a
+cursor = lens (\(Zip _ (a:_)) -> a) $ \(Zip ls (_:rs)) a -> Zip ls (a:rs)
