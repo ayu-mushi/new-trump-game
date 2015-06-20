c05cb88ded94be08a9ace323feb24e43fe49f27d
ayu-mushi <ayu.mushi2015@gmail.com>
2015-06-21 01:31:49 +0900

    added structures 'ComputerPlayer' & 'HumanPlayer'

--- NewTrumpGame/Player.hs
+++ NewTrumpGame/Player.hs
@@ -11,5 +11,14 @@ data Player = Player {
 hand :: Lens' Player [Card]; hand = lens _hand $ \p x -> p { _hand = x}
 deck :: Lens' Player [Card]; deck = lens _deck $ \p x -> p { _deck = x}
 
+data ComputerPlayer = ComputerPlayer {
+  cpHand :: [Card],
+  cpDeck :: [Card]
+  }
+data HumanPlayer = HumanPlayer {
+  humanHand :: ([Card], [Card]),
+  humanDeck :: [Card]
+  }
+
 initialDraw :: [Card] -> Player
 initialDraw deck = Player (take 3 deck) (drop 3 deck)
