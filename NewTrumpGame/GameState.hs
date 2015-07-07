3212a708e2dc718e29a284efc94cc217d35f2a6a
ayu-mushi <ayu.mushi2015@gmail.com>
2015-07-07 20:22:37 +0900

    add ZipicAccessorsMul & use that for controling sucrifices

--- NewTrumpGame/GameState.hs
+++ NewTrumpGame/GameState.hs
@@ -20,20 +20,16 @@ instance P.ToElem Field where
         Just card -> show card
 
 type ZipicAccessor a = [a] -> ([a], [a])
-
 zipNext :: ZipicAccessor a -> ZipicAccessor a
 zipNext za xs = ((a:ls), rs)
   where (ls, (a:rs)) = za xs
-
 zipFirst :: ZipicAccessor a -> ZipicAccessor a
 zipFirst za xs = ([], (reverse ls) ++ rs)
   where (ls, rs) = za xs
 
-applyZa :: ZipicAccessor a -> [a] -> [a]
-applyZa za xs = (reverse ls) ++ rs
-  where (ls, rs) = za xs
+type ZipicAccessorsMul a = [a] -> [[a]]
 
-data Phase = Draw | Hand | Sacrifice (Maybe (ZipicAccessor Card)) | Summon (Maybe (ZipicAccessor Card)) [ZipicAccessor Card] | End
+data Phase = Draw | Hand | Sacrifice (Maybe (ZipicAccessor Card)) | Summon (Maybe (ZipicAccessor Card)) (ZipicAccessorsMul Card) | End
 
 instance Show Phase where
   show p = case p of
