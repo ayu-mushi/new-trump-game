eba35a1b599c9fee1a1c091003055572e91341b2
ayu-mushi <ayu.mushi2015@gmail.com>
2015-07-10 22:58:49 +0900

    rm color

--- NewTrumpGame/Cards.hs
+++ NewTrumpGame/Cards.hs
@@ -6,30 +6,28 @@ module NewTrumpGame.Cards
     initDeck) where
 import System.Random.Shuffle (shuffle')
 import System.Random (RandomGen)
+import Data.Maybe (isNothing)
 
-newtype Color = Color { index :: Int }
-
-newtype Card = Card { fromCard :: Maybe Color }
+newtype Card = Card { fromCard :: Maybe Int }
 
 instance Show Card where
-  show (Card Nothing)           = "w"
-  show (Card (Just (Color 1)))  = "A"
-  show (Card (Just (Color 11))) = "J"
-  show (Card (Just (Color 12))) = "Q"
-  show (Card (Just (Color 13))) = "K"
-  show (Card (Just (Color i)))  = show i
+  show (Card Nothing)   = "w"
+  show (Card (Just 1))  = "A"
+  show (Card (Just 11)) = "J"
+  show (Card (Just 12)) = "Q"
+  show (Card (Just 13)) = "K"
+  show (Card (Just i))  = show i
 
 isColored :: Card -> Bool
-isColored (Card Nothing)  = False
-isColored (Card (Just _)) = True
+isColored = isNothing . fromCard
 
 energy :: Card -> Int
 energy (Card Nothing) = 2
 energy _              = 1
 
-cost :: Color -> Int
-cost (Color i) = if i > 10 then 2 else 0
+cost :: Int -> Int
+cost i = if i > 10 then 2 else 0
 
 initDeck :: RandomGen g => g -> [Card]
 initDeck g = shuffle' allCards (length allCards) g
-  where allCards = concat $ replicate 4 $ map Card $ Nothing : (map (Just . Color) [1..13])
+  where allCards = concat $ replicate 4 $ map Card $ Nothing : (map Just [1..13])
