68959862742d74572f447f64052c3b3f21ede988
ayu-mushi <ayu.mushi2015@gmail.com>
2015-07-24 17:48:39 +0900

    separete part of main.hs to 'Util.hs'

---
+++ NewTrumpGame/Util.hs
@@ -0,0 +1,60 @@
+module NewTrumpGame.Util where
+
+import qualified Haste.Perch as P
+import Haste (alert, Elem, toJSString, Event(OnClick), evtName, setTimeout)
+import Haste.Foreign (ffi)
+
+tagName :: Elem -> IO String
+tagName = ffi $ toJSString "(function(e){ return e.tagName })"
+
+prevElem :: Elem -> IO (Maybe Elem)
+prevElem el = do
+  prevNode <- jsPreviousSibling el
+  is0 <- isNull prevNode
+  if is0
+    then return Nothing
+    else do
+      isEl <- isElementNode prevNode
+      if isEl
+        then return $ Just prevNode
+        else prevElem prevNode
+  where
+    jsPreviousSibling = ffi $ toJSString "(function(e){ return e.previousSibling })"
+    isNull = ffi $ toJSString "(function(x) {return x === null})"
+    isElementNode = ffi $ toJSString "(function(node) {return node.nodeType === 1})"
+
+indexEl :: Int -> Elem -> IO Int
+indexEl z tag = do
+  tag' <- prevElem tag
+  case tag' of
+    Nothing -> return z
+    Just el -> indexEl (succ z) el
+
+indexOfParentEl :: Elem -> IO Int
+indexOfParentEl = (>>=indexEl 0) . P.parent
+
+forTargetWhenEvt :: Event IO a -> (Elem -> IO ()) -> P.Perch
+forTargetWhenEvt event action = P.Perch $ \e -> do {jsAddEventListener e (evtName event) action; return e}
+  where
+    jsAddEventListener :: Elem -> String -> (Elem -> IO ()) -> IO ()
+    jsAddEventListener = ffi $ toJSString "(function(node, evtName, f){ node.addEventListener(evtName, (function(e){ f(e.target) }))})"
+
+forIndexOfClickedLiElem :: (Int -> IO ()) -> P.Perch
+forIndexOfClickedLiElem f = forTargetWhenEvt OnClick $
+  \el -> do
+    tn <- tagName el
+    if tn == "LI"
+      then indexEl 0 el >>= f
+      else return ()
+
+forIndexOfClickedTdElem :: (Int -> Int -> IO ()) -> P.Perch
+forIndexOfClickedTdElem f = forTargetWhenEvt OnClick $
+  \el -> do
+    tn <- tagName el
+    if tn == "TD"
+      then do
+        x <- indexOfParentEl el
+        y <- indexEl 0 el
+        f x y
+      else
+        return ()
--- main.hs
+++ main.hs
@@ -14,61 +14,7 @@ import System.Random (mkStdGen)
 import NewTrumpGame.GameState
 import NewTrumpGame.Player
 import NewTrumpGame.Cards
-
-tagName :: Elem -> IO String
-tagName = ffi $ toJSString "(function(e){ return e.tagName })"
-
-prevElem :: Elem -> IO (Maybe Elem)
-prevElem el = do
-  prevNode <- jsPreviousSibling el
-  is0 <- isNull prevNode
-  if is0
-    then return Nothing
-    else do
-      isEl <- isElementNode prevNode
-      if isEl
-        then return $ Just prevNode
-        else prevElem prevNode
-  where
-    jsPreviousSibling = ffi $ toJSString "(function(e){ return e.previousSibling })"
-    isNull = ffi $ toJSString "(function(x) {return x === null})"
-    isElementNode = ffi $ toJSString "(function(node) {return node.nodeType === 1})"
-
-indexEl :: Int -> Elem -> IO Int
-indexEl z tag = do
-  tag' <- prevElem tag
-  case tag' of
-    Nothing -> return z
-    Just el -> indexEl (succ z) el
-
-indexOfParentEl :: Elem -> IO Int
-indexOfParentEl = (>>=indexEl 0) . P.parent
-
-forTargetWhenEvt :: Event IO a -> (Elem -> IO ()) -> P.Perch
-forTargetWhenEvt event action = P.Perch $ \e -> do {jsAddEventListener e (evtName event) action; return e}
-  where
-    jsAddEventListener :: Elem -> String -> (Elem -> IO ()) -> IO ()
-    jsAddEventListener = ffi $ toJSString "(function(node, evtName, f){ node.addEventListener(evtName, (function(e){ f(e.target) }))})"
-
-forIndexOfClickedLiElem :: (Int -> IO ()) -> P.Perch
-forIndexOfClickedLiElem f = forTargetWhenEvt OnClick $
-  \el -> do
-    tn <- tagName el
-    if tn == "LI"
-      then indexEl 0 el >>= f
-      else return ()
-
-forIndexOfClickedTdElem :: (Int -> Int -> IO ()) -> P.Perch
-forIndexOfClickedTdElem f = forTargetWhenEvt OnClick $
-  \el -> do
-    tn <- tagName el
-    if tn == "TD"
-      then do
-        x <- indexOfParentEl el
-        y <- indexEl 0 el
-        f x y
-      else
-        return ()
+import NewTrumpGame.Util
 
 refresh :: MVar Game -> IO ()
 refresh reftoGame = void $
