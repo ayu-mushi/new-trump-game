b64fa956a2bc020455f04254e9ed0671e91a6856
ayu-mushi <ayu.mushi2015@gmail.com>
2015-07-30 00:39:27 +0900

    refactor from 'sequence map 〜' to 'mapM 〜'

--- Game/BoardTrump/GameState.hs
+++ Game/BoardTrump/GameState.hs
@@ -228,7 +228,7 @@ instance P.ToElem Game where
           fieldTrs <- elemsByQS e "#field tr"
           sbjTd <- fmap (!! (sbjOfMv ^. _2)) $ elemsByQS (fieldTrs !! (sbjOfMv^._1)) "td"
           setAttr sbjTd "id" "moving-subject"
-          fieldTdss <- sequence $ map (elemsByQS `flip` "td") fieldTrs
+          fieldTdss <- mapM (elemsByQS `flip` "td") fieldTrs
           mapM_ (setClass `flip` "motion-scope" `flip` True) $
             (map (\possibleMoving -> fieldTdss ^. ix (possibleMoving sbjOfMv ^. _1) . ix (possibleMoving sbjOfMv ^. _2))
               $ filter (uncurry (uncurry (isMovable game) sbjOfMv) . ($ sbjOfMv))
