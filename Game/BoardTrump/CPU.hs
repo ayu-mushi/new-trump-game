9bf5fedea4a87af7926a541c1fa2514c34d806e6
ayu-mushi <ayu.mushi2015@gmail.com>
2015-07-30 03:58:25 +0900

    add what about field when main phase to possiblePlay

--- Game/BoardTrump/CPU.hs
+++ Game/BoardTrump/CPU.hs
@@ -2,7 +2,7 @@ module Game.BoardTrump.CPU () where
 
 import Lens.Family2
 import Lens.Family2.Stock (_2)
-import Data.Maybe (fromJust, mapMaybe, isJust)
+import Data.Maybe (fromJust, mapMaybe, catMaybes, isJust)
 
 import Game.BoardTrump.Player (hand)
 import Game.BoardTrump.GameState
@@ -15,7 +15,7 @@ advantage = undefined
 possiblePlay :: Game -> [Play]
 possiblePlay game =
   case game ^. phase of
-    Main -> []
+    Main -> (map (Play . Right) $ catMaybes $ concat $ forPlaneWithIx (game ^. field) $ \i j m -> case m of Just (p, c) -> if (p == (game ^. isYourTurn)) && (not $ null $ movableZone c game i j) then Just (i, j) else Nothing; Nothing -> Nothing)
       ++ (map (Play . Left) $ mapMaybe (\(i, p) -> if p then Just i else Nothing) $ zip [0..] $ map (isSelectable game) $ game ^. turnPlayer . hand)
     Sacrifice cost -> map (Play . Left) [0..(pred $ length $ game ^. turnPlayer . hand)]
     Move (x, y) -> map (Play . Right) $ movableZone (view _2 $ fromJust $ game ^. field . cell x y) game x y
