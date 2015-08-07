fea9e5b79a57d0331725acc9cc73900b790eb1dc
ayu-mushi <ayu.mushi2015@gmail.com>
2015-08-07 21:40:52 +0900

    fix

--- Game/BoardTrump/CPU.hs
+++ Game/BoardTrump/CPU.hs
@@ -2,7 +2,7 @@ module Game.BoardTrump.CPU (randomly, runPlay) where
 
 import Lens.Family2
 import Lens.Family2.Stock (_2)
-import Data.Maybe (fromJust, mapMaybe, catMaybes, isNothing)
+import Data.Maybe (fromJust, mapMaybe, catMaybes, isNothing, isJust)
 import System.Random (Random(randomR), StdGen)
 
 import Game.BoardTrump.Player (hand)
@@ -18,8 +18,8 @@ possiblePlay :: Game -> [Play]
 possiblePlay game =
   case game ^. phase of
     Main ->
-      (if null $ game ^. field . summonableZone (game ^. isYourTurn) then [] else (map (Play . Right) $ catMaybes $ concat $ forPlaneWithIx (game ^. field) $ \i j m -> case m of Just (p, c) -> if (p == (game ^. isYourTurn)) && (not $ null $ movableZone c game i j) then Just (i, j) else Nothing; Nothing -> Nothing))
-      ++ (map (Play . Left) $ mapMaybe (\(i, p) -> if p then Just i else Nothing) $ zip [0..] $ map (isSummonable game) $ game ^. turnPlayer . hand)
+      (map (Play . Right) $ catMaybes $ concat $ forPlaneWithIx (game ^. field) $ \i j m -> case m of Just (p, c) -> if (p == (game ^. isYourTurn)) && (not $ null $ movableZone c game i j) then Just (i, j) else Nothing; Nothing -> Nothing)
+      ++ (if all isJust $ game ^. field . summonableZone (game ^. isYourTurn) then [] else (map (Play . Left) $ mapMaybe (\(i, p) -> if p then Just i else Nothing) $ zip [0..] $ map (isSummonable game) $ game ^. turnPlayer . hand))
     Sacrifice cost -> map (Play . Left) [0..(pred $ length $ game ^. turnPlayer . hand)]
     Move (x, y) -> map (Play . Right) $ movableZone (view _2 $ fromJust $ game ^. field . cell x y) game x y
     Summon obj -> map (Play . Right . (,) (if game^.isYourTurn then pred $ length $ game ^. field else 0)) $ mapMaybe (\(i, m) -> if isNothing m then Just i else Nothing) $ zip [0..] $ game ^. field . summonableZone (game ^. isYourTurn)
