69e61caaa906b8a0234d321c932461985003f104
ayu-mushi <ayu.mushi2015@gmail.com>
2015-08-21 07:46:23 +0900

    add a action 'Pass'

--- Game/BoardTrump/CPU.hs
+++ Game/BoardTrump/CPU.hs
@@ -9,21 +9,21 @@ import Game.BoardTrump.Player (hand)
 import Game.BoardTrump.GameState
 import Game.BoardTrump.Util (ix, forPlaneWithIx)
 
-data Play = WithHand Int | WithField (Int, Int) deriving Show
+data Play = WithHand Int | WithField (Int, Int) | Pass deriving Show
 
 advantage :: Game -> Int
 advantage = undefined
 
 possiblePlay :: Game -> [Play]
-possiblePlay game =
-  case game ^. phase of
+possiblePlay game = Pass:
+  (case game ^. phase of
     Main ->
       (map WithField $ catMaybes $ concat $ forPlaneWithIx (game ^. field) $ \i j m -> case m of Just (p, c) -> if (p == (game ^. isYourTurn)) && (not $ null $ movableZone c game i j) then Just (i, j) else Nothing; Nothing -> Nothing)
       ++ (if all isJust $ game ^. field . summonableZone (game ^. isYourTurn) then [] else (map (WithHand) $ mapMaybe (\(i, p) -> if p then Just i else Nothing) $ zip [0..] $ map (isSummonable game) $ game ^. turnPlayer . hand))
     Sacrifice cost -> map (WithHand) [0..(pred $ length $ game ^. turnPlayer . hand)]
     Move (x, y) -> map (WithField) $ movableZone (view _2 $ fromJust $ game ^. field . cell x y) game x y
     Summon obj -> map (WithField . (,) (if game^.isYourTurn then pred $ length $ game ^. field else 0)) $ mapMaybe (\(i, m) -> if isNothing m then Just i else Nothing) $ zip [0..] $ game ^. field . summonableZone (game ^. isYourTurn)
-    _ -> []
+    _ -> [])
 
 randomly :: Game -> (Play, StdGen)
 randomly game =
@@ -34,3 +34,4 @@ runPlay :: Play -> Game -> Game
 runPlay play game = case play of
   WithHand i -> operateWithHand i game
   WithField (i, j) -> operateWithField i j game
+  Pass -> game & phase .~ Wait
