55852d3dd1da636d13917a5f37ed445975931486
ayu-mushi <ayu.mushi2015@gmail.com>
2015-08-26 08:47:55 +0900

    add a module 'Game.BoardTrump' pathed Game/BoardTrump.hs

---
+++ Game/BoardTrump.hs
@@ -0,0 +1,9 @@
+module Game.BoardTrump (
+  module Game.BoardTrump.GameState
+  ) where
+
+import Game.BoardTrump.GameState
+import Game.BoardTrump.Player
+import Game.BoardTrump.Cards
+import Game.BoardTrump.Util
+import Game.BoardTrump.CPU
